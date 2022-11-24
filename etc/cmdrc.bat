8f437996991372eb6ea097742e14c0c4dd314784
099B25036D434A400086A4A63C00EDF85FFCC0B8 <unknown>
2022-11-24 15:31:17 +0900

    win

--- etc/cmdrc.bat
+++ etc/cmdrc.bat
@@ -1,4 +1,15 @@
 @echo off
 
-if defined _CMDRC goto :eof
-set _CMDRC=1
+  reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" ^
+    /v AutoRun ^
+    /t REG_SZ  ^
+    /d "%~0"   ^
+    /f > nul
+
+  set "JAVA_HOME=%USERPROFILE%/opt/net.java.jdk/19"
+  set "PATH=%PATH%;%JAVA_HOME%/bin"
+
+  set "CLASSPATH=.;./target/classes;./target/dependency/*;%USERPROFILE%/lib/*"
+
+:EOF
+echo on
