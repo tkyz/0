dfdd01c101cf75d86f7bd2db3a0dc2f75c680d58
unknown <unknown>
2024-05-24 07:47:25 +0900

    unknown

--- entrypoint.py
+++ entrypoint.py
@@ -1,49 +1,73 @@
 import os
 import traceback
-import pprint
 import re
 import glob
+import subprocess
+import pathlib
 
 class Main:
 
-  def walk():
+  class Blobfs:
 
-    type_cnt = {}
+    def __init__(self, base_dir):
+      self.base_dir = os.path.abspath(base_dir)
 
-    for path in glob.iglob('./**/*', recursive=True):
+    def ext(self, path):
 
-      if os.path.isdir(path):
-        continue
-      if os.path.islink(path):
-        continue
+      ext = None
 
-      st_nlink  = os.stat(path).st_nlink
-      ext       = None
-      mime_type = None
+      if re.fullmatch(self.base_dir + r'/.*\.([^/\.]+)',        path):
+        ext = re.sub( self.base_dir + r'/.*\.([^/\.]+)', r'\1', path).lower()
+        ext = ext.lower()
 
-      if re.fullmatch(r'.*\.([^/\.]+)', path):
-        ext = re.sub( r'.*\.([^/\.]+)', r'\1', path).lower()
+      return ext
 
-      # TODO: mime_type
-      if False:
-        pass
+    def mime_type(self, path):
 
-      if ext is None:
-        continue
-      if ext.startswith('test_'):
-        continue
-      if ext in ['license', 'template']:
-        continue
+      ret = subprocess.run(['file', '--brief', '--mime-type', path], capture_output = True, text = True)
+      if 0 == ret.returncode:
 
-      cnt = type_cnt.get(ext)
-      cnt = cnt if cnt is not None else 0
-      type_cnt[ext] = cnt + 1
+        mime_type = re.sub(r'[\r\n]+', '', ret.stdout)
+        mime_type = mime_type.lower()
 
-    pprint.pprint(type_cnt)
+        blob_dir = self.base_dir + '/ref/mime_type/' + mime_type
+        os.makedirs(blob_dir, exist_ok=True)
+
+      return mime_type
+
+    def run(self):
+
+      sub_dirs = []
+      sub_dirs.append('/blob/sha256')
+      sub_dirs.append('/ref')
+
+      for sub_dir in sub_dirs:
+        for path in glob.iglob(self.base_dir + sub_dir + '/**/*', recursive=True):
+
+          if not os.path.isfile(path):
+            continue
+          if os.path.islink(path):
+            continue
+          if re.fullmatch(self.base_dir + r'/blob/sha256/([0-9a-f]{2}/)*([0-9a-f]{64})\.tmp/.*', path):
+            continue
+
+          st_nlink  = os.stat(path).st_nlink
+          ext       = self.ext(path)
+          mime_type = self.mime_type(path)
+
+          if mime_type == 'inode/x-empty':
+            continue
+          if mime_type.startswith('text'):
+            continue
+
+#         if 1 == st_nlink:
+#           print(str(st_nlink) + ' ' + str(ext) + ' ' + path)
+
+          print(str(st_nlink) + ' ' + str(mime_type) + ' ' + str(ext) + ' ' + path)
 
   def run():
     try:
-      Main.walk()
+      Main.Blobfs().run()
     except:
       traceback.print_exc()
 
